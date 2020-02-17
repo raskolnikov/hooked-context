93738da9fb470ebf2c06366fc35f65ce6a2e84e0
Mehmet Aktas <mehmet.aktas@givergy.com>
2020-02-17 10:08:08 +0000

    Updates

---
+++ precache-manifest.be4ef7116748ad46a9fb59c68782ea36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a33e3fa2a2e0d0be0818f55029edbe",
+    "url": "/hooked-context/index.html"
+  },
+  {
+    "revision": "7d8be23a24a6bf2f747f",
+    "url": "/hooked-context/static/css/main.fd69f45d.chunk.css"
+  },
+  {
+    "revision": "b6d3d27767ab71e3f6f9",
+    "url": "/hooked-context/static/js/2.2df6e2f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hooked-context/static/js/2.2df6e2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8be23a24a6bf2f747f",
+    "url": "/hooked-context/static/js/main.21ce2890.chunk.js"
+  },
+  {
+    "revision": "55e2fb5666f9252fd8ea",
+    "url": "/hooked-context/static/js/runtime-main.e90bcfa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hooked-context/static/media/logo.5d5d9eef.svg"
+  }
+]);
